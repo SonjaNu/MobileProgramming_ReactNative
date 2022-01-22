bce9ffff1edc465725f2d72eb0881e9ef03b8f14
sonja <sonjeli93@gmail.com>
2022-01-22 17:11:09 +0200

    add calculator and number guessing game

---
+++ calculator/App.js
@@ -0,0 +1,75 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, Alert, TextInput, Image} from 'react-native';
+
+export default function CalculatorApp() {
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'pink',
+      alignItems: 'center',
+      justifyContent: 'center',
+      
+    },
+
+    text: {
+      marginBottom: 20,
+
+    },
+
+    input: {
+      width:80, 
+      borderColor:'purple', 
+      borderWidth:2,
+      color: 'purple',
+      justifyContent: 'space-around', //how to get space between two textfields...?
+      
+    }, 
+    operators: {
+    marginTop: 20,
+    flexDirection:'row', 
+    justifyContent: 'space-around',  //How to get space between two buttons...?
+
+    },
+  });
+  
+
+  const subtraction= () =>{
+    setResult(Number(count) - Number(count2));
+    setCount('')
+    setCount2('')
+  }
+
+  const addition= () =>{
+    setResult(Number(count) + Number(count2));
+    setCount('')
+    setCount2('')
+  }
+
+  const [count, setCount] = useState('');
+  const [count2, setCount2] = useState('');
+  const [result, setResult] = useState('');
+  
+  return (
+    
+    <View style={styles.container}>
+      <Text style={styles.text} color="purple" >Result: {result}  </Text>
+      <TextInput style={styles.input}
+        keyboardType= 'number-pad'
+        onChangeText={text=> setCount(text)}
+        value={count}/>
+      <TextInput style={styles.input}
+        keyboardType= 'number-pad'
+        onChangeText={text=> setCount2(text)}
+        value={count2}/>
+      <View  style={styles.operators}>
+        <Button  onPress={addition} title="+" color="purple" /> 
+        <Button onPress={subtraction} title="-" color="purple"/> 
+
+      </View>
+
+    </View>
+  );
+}
+
---
+++ guessnumber/App.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+
+export default function GuessNumberApp() {
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: '#b8a9c9',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+    },
+    button: {
+      flex: 1,
+      borderColor:'purple',
+      paddingTop: 20,
+      width: 150
+    },
+  });
+
+  const [correctNumber, setCorrectNumber] = React.useState(Math.floor(Math.random() * 100) + 1);
+  const [guessedNumber, setGuessedNumber] = React.useState('');
+  const [guess, setGuess] = React.useState(1);
+  const [responseText, setResponseText] = React.useState('Guess a number between 1-100');
+
+  const guessSomeNumber = () => {
+    setGuess (guess + 1);
+
+    if(guessedNumber < correctNumber) {
+      setResponseText('Your guess ' + guessedNumber + ' is too low')
+    } 
+    
+    if(guessedNumber > correctNumber) {
+      setResponseText('Your guess ' + guessedNumber + ' is too high')
+    } 
+
+    if(guessedNumber == correctNumber) {
+      setResponseText('Your guess ' + guessedNumber + ' is correct')
+      Alert.alert('You guessed the number in ' + guess + ' guesses')
+    }
+
+  }
+
+  return (
+  <View style={styles.container}>
+
+    <View style={styles.input}>
+
+      <Text style={{fontSize: 18, color: 'purple', paddingBottom: 10}}>{responseText}</Text>
+    
+      <TextInput style={{width:50,  borderWidth:2, borderColor:'purple',}}
+      onChangeText={guessNumber => setGuessedNumber(guessNumber)}
+      value={guessedNumber}
+      keyboardType='numeric'/>
+
+    </View>
+
+    <View style={styles.button}>
+
+      <Button color="purple" onPress={guessSomeNumber} title="MAKE GUESS"/>
+
+    </View>
+  </View>
+  );
+}
+
